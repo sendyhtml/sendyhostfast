bc082e2f231be2218c7f3b623faa0d6be5b5e598
sendyhtml <sendyfalisha@gmail.com>
2025-12-31 11:13:45 +0700

    Add files via upload

---
+++ api/random-video.js
@@ -0,0 +1,15 @@
+module.exports = {
+  name: "Random Video",
+  desc: "video random",
+  category: "Random",
+  path: "/random/randomvid",
+  async run(req, res) {
+
+    const buffer = await getBuffer(`https://api.fikmydomainsz.xyz/random/randomvid`);
+    res.writeHead(200, {
+      'Content-Type': 'video/video',
+      'Content-Length': buffer.length,
+    });
+    res.end(buffer);
+  }
+}
